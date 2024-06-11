2d97dcf30379a95da5a8f82b9992299abcf4b30f
aminekh001 <147535837+aminekh001@users.noreply.github.com>
2024-06-11 17:42:29 +0100

    Update db.js

--- backend/dbs/db.js
+++ backend/dbs/db.js
@@ -4,7 +4,7 @@ const connectDB=async ()=>{
     try{
         
         await mongoose.connect("mongodb://localhost:27017/reservation")
-        //("mongodb+srv://aminekhairi88:SOCIAL@cluster0.o4ze18s.mongodb.net/reservation?retryWrites=true&w=majority&appName=Cluster0")
+        
         
 
         console.log("db connected  successfully!!")
